df880bea8040fc997ce2603a20677d1ed3abb1a6
Itumeleng More <113109028+Moreitu1985@users.noreply.github.com>
2026-04-14 09:33:24 +0200

    Update database password in db.js

--- devtrack_app/backend/db.js
+++ devtrack_app/backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'More@14moengit',
+  password: 'password for your database',
   database: 'devtrack'
 });
 
